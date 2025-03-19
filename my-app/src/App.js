c052812f45a59be883c5e16e93911fc5c44e5c66
Akshaya_289868 <akshayagorrepati868@gmail.com>
2025-03-19 11:28:25 +0530

    g

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Products from "./components/Products";
+function App(props) {
+  return (
+    <>
+      <Header/>
+      <Products/>
+      <Footer/>
+    </>
+  );
+}
+export default App;
